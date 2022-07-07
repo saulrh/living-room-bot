1a71b1db0b83450c64728afb94d00b6f9a4ce0fa
Saul Reynolds-Haertle <saul@saulrh.com>
2022-07-07 16:29:55 -0700

    Wait ten seconds before notifying
    This prevents accidental joins (misclicks etc) from causing unwanted notifications.

--- src/living_room_bot/main.py
+++ src/living_room_bot/main.py
@@ -6,6 +6,7 @@ import apscheduler.jobstores.memory
 import apscheduler.schedulers.asyncio
 import apscheduler.schedulers.base
 import apscheduler.triggers.interval
+import apscheduler.triggers.date
 
 
 class ChannelNotFoundError(Exception):
@@ -19,6 +20,8 @@ class _LivingRoomClient(discord.Client):
         voice_id: int,
         gc_after: datetime.timedelta,
         gc_horizon: datetime.timedelta,
+        sched: apscheduler.schedulers.base.BaseScheduler,
+        debounce_period: datetime.timedelta,
         *args,
         **kwargs,
     ):
@@ -27,6 +30,8 @@ class _LivingRoomClient(discord.Client):
         self._text_id = text_id
         self._gc_after = gc_after
         self._gc_horizon = gc_horizon
+        self._sched = sched
+        self._debounce_period = debounce_period
 
     @property
     def text_channel(self) -> discord.TextChannel:
@@ -41,13 +46,20 @@ class _LivingRoomClient(discord.Client):
         before: discord.VoiceState,
         after: discord.VoiceState,
     ):
-        del member # unused
         if after.channel:
             user_changed_channels = after.channel != before.channel
             only_person_in_channel = len(after.channel.members) == 1
             in_living_room = after.channel.id == self._voice_id
-            if only_person_in_channel and in_living_room:
-                await self.text_channel.send(f"Someone's in the living room!")
+            if user_changed_channels and only_person_in_channel and in_living_room:
+                bounce_check_time = (
+                    datetime.datetime.now(tz=datetime.timezone.utc)
+                    + self._debounce_period
+                )
+                self._sched.add_job(
+                    func=self.check_debounce,
+                    trigger=apscheduler.triggers.date.DateTrigger(bounce_check_time),
+                    args=[member, after.channel],
+                )
 
     async def clean_up_old_notifications(self):
         start_time = (
@@ -59,6 +71,12 @@ class _LivingRoomClient(discord.Client):
         async for message in self.text_channel.history(limit=1000, after=start_time):
             if message.author == self.user and message.created_at < gc_horizon:
                 await message.delete()
+
+    async def check_debounce(
+        self, member: discord.Member, channel: discord.VoiceChannel
+    ):
+        if member in channel.members:
+            await self.text_channel.send(f"Someone's in the living room!")
 
 
 _CLIENT = None
@@ -127,6 +145,13 @@ def make_scheduler(*args, **kwargs):
     default=60 * 60,
     help="Int seconds beyond which old notifications will be garbage collected",
 )
+@click.option(
+    "--debounce_period",
+    envvar="DEBOUNCE_PERIOD",
+    type=int,
+    default=10,
+    help="Only register joins if the user stays in the channel for this many seconds",
+)
 def run(
     discord_bot_token: str,
     voice_id: int,
@@ -134,6 +159,7 @@ def run(
     message_gc_frequency: int,
     message_gc_horizon: int,
     message_gc_after: int,
+    debounce_period: int,
 ):
     sched = make_scheduler()
     client = get_client(
@@ -141,11 +167,13 @@ def run(
         voice_id=voice_id,
         gc_after=datetime.timedelta(seconds=message_gc_after),
         gc_horizon=datetime.timedelta(seconds=message_gc_horizon),
+        sched=sched,
+        debounce_period=datetime.timedelta(seconds=debounce_period),
     )
     sched.add_job(
         func=client.clean_up_old_notifications,
         trigger=apscheduler.triggers.interval.IntervalTrigger(
-            seconds=message_gc_frequency
+            seconds=message_gc_frequency,
         ),
         args=[],
         id="periodic_garbage_collect",
